c541acf928632a9546e4909cc8862576623701d8
Enderic <mahiyan21@gmail.com>
2026-09-15 18:05:54 -0500

    set up top level module

--- mnist_fpga/mnist_fpga.srcs/sources_1/new/accel_top.sv
+++ mnist_fpga/mnist_fpga.srcs/sources_1/new/accel_top.sv
@@ -15,6 +15,42 @@ module accel_top(
 
     top_state state;
 
+    logic conv_en, pool_en;
+
+    // UART Protocol will go here
+
+    conv_layer conv(
+        .clk(clk),
+        .rst_n(rst_n),
+        .en(),
+
+        .img_wr_en(),
+        .img_wr_addr(),
+        .img_wr_data(),
+
+        .calc_v(),
+        .final_res(),
+        .res_addr(),
+        .filter_addr(),
+        .done()
+    );
+
+    max_pool pool(
+        .clk(clk),
+        .rst_n(rst_n),
+        .en(),
+
+        .wr_en(),
+        .wr_data(),
+        .wr_mem_addr(),
+        .wr_filter_addr(),
+
+        .result(),
+        .res_addr(),
+        .res_v(),
+        .done()
+    );
+
     always_ff @ (posedge clk) begin
         if(!rst_n) begin
             state <= TOP_IDLE;
